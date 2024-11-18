7960b186c15fdf2350c987d8754cf80a31b0cf47
Amritha <amritharnair2@gmail.com>
2024-11-18 21:36:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// Using getElementById
+const mainTitle = document.getElementById('main-title');
+console.log(mainTitle);
+
+// Using getElementsByClassName
+const containers = document.getElementsByClassName('container');
+console.log(containers);
+
+const sections = document.getElementsByClassName('section');
+console.log(sections);
+
+// Using getElementsByTagName
+const paragraphs = document.getElementsByTagName('p');
+console.log(paragraphs);
+
+// Using querySelector
+const firstButton = document.querySelector('.btn');
+console.log(firstButton);
+
+const firstSection = document.querySelector('.section');
+console.log(firstSection);
+
+// Using querySelectorAll
+const allButtons = document.querySelectorAll('.btn');
+console.log(allButtons);
+
+const containerParagraphs = document.querySelectorAll('.container p');
+console.log(containerParagraphs);
